b1eb64d756ec5864de11f8ec1cc0041278cd81ec
OgabekRakhimboev <ogabekrakhimboev01@gmail.com>
2022-08-15 19:58:46 +0500

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+
+    public static void main(String [] args){
+        divide(19);
+    }
+    public static void divide(double toBeDivided) {
+        System.out.println(toBeDivided/5);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void main(String [] args){
+        convert(11.5F);
+    }
+
+    public static void convert(float minutes) {
+        System.out.println((minutes*60));
     }
 }
